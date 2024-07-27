b786942032656438316ac35e8007231437b958b1
Alejandro Di Stefano <addistefano@live.com.ar>
2024-07-26 21:01:07 -0300

    Progress Bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,37 @@
-// Boton de modo oscuro
-const switchButton = document.getElementById('switch');
+document.addEventListener('DOMContentLoaded', function () {
+    // Boton de modo oscuro
+    const switchButton = document.getElementById('switch');
 
-// Verificar el modo guardado en localStorage
-if (localStorage.getItem('Mode') === 'Dark') {
-    document.body.classList.add('dark');
-    switchButton.classList.add('active');
-} else {
-    document.body.classList.remove('dark');
-    switchButton.classList.remove('active');
-}
-
-
-// Agregar el evento click al botón
-switchButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-    switchButton.classList.toggle('active');
-
-    if (document.body.classList.contains('dark')) {
-        localStorage.setItem('Mode', 'Dark');
+    // Verificar el modo guardado en localStorage
+    if (localStorage.getItem('Mode') === 'Dark') {
+        document.body.classList.add('dark');
+        switchButton.classList.add('active');
     } else {
-        localStorage.removeItem('Mode');
+        document.body.classList.remove('dark');
+        switchButton.classList.remove('active');
     }
+
+    // Agregar el evento click al botón
+    switchButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark');
+        switchButton.classList.toggle('active');
+
+        if (document.body.classList.contains('dark')) {
+            localStorage.setItem('Mode', 'Dark');
+        } else {
+            localStorage.removeItem('Mode');
+        }
+    });
+
+    const rangeInput = document.getElementById('satisfaccion');
+    const progressBar = document.querySelector('.progress-bar');
+    const progressP = document.getElementById('progress_p');
+
+    rangeInput.addEventListener('input', function () {
+        const value = (rangeInput.value - rangeInput.min) / (rangeInput.max - rangeInput.min) * 100;
+        progressBar.style.maxWidth = '95%';
+        progressBar.style.width = `${value}%`;
+        const valorParceado = parseInt(rangeInput.value);
+        progressP.innerHTML = `${valorParceado}%`;
+    });
 });
